fa8cf1c764dd5116bbacb43336a78c3fb88d85d9
AFIQ AIMAN <afiqaiman091@gmail.com>
2022-02-14 23:25:50 +0800

    seeder databse

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Engineer;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+
+            'name' => 'admin',
+            'email' =>'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'usertype'=>1
+
+        ]);
+
+        User::create([
+
+            'name' => 'user',
+            'email' =>'user@gmail.com',
+            'password' => Hash::make('12345678'),
+            'usertype'=>0
+
+        ]);
+
+        Engineer::create([
+            'name' => 'engineer',
+            'email' =>'engineer@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
